80d5cd5d29304d2e3ab6e320f3cd164bc1afd053
ssdivyaravali <ssdivyaravali@gmail.com>
2026-01-15 00:59:30 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from template import project_name
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Image-Classification"
+AUTHOR_USER_NAME = "SSDivyaRavali"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "useremail"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description=f"A small python package for {SRC_REPO} app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
